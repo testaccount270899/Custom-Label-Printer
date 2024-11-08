0db12c556dea400ef14a397de94261c36efbcd34
testaccount270899 <sdjsdgj@moneymine.club>
2024-11-07 20:23:26 -0500

    Add files via upload

---
+++ whistle6pro.bat
@@ -0,0 +1,9 @@
+@echo off
+set gwtn_term=1
+set gwtn_graphics=2
+set gwtn_color=0
+
+rem \\WhistleClient\GSWClient\GS_clnt /a /hWhistleClient
+\\WhistleClient\GSWClient\GS_CLNT  /hWhistleClient -usvcSCEFork -pB!gchicken -dpoultry
+rem \\OKWPROWHS01\GSWClient\GS_CLNT  /hOKWPROWHS01 -usvcSCEFork -pB!gchicken -dpoultry
+
